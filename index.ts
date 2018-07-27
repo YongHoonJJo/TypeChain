dc926f68b9b6a3e7409e2777ba7362c3ed8fd0c3
YongHoonJJo <starctak@gmail.com>
2018-07-27 15:14:21 +0900

    #3 First steps with Typescript // setting optional argument

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ const name = "YongHoonJJo",
     age = 24,
     gender = "male";
 
-const sayHi = (name, age, gender) => {
+const sayHi = (name, age, gender?) => {
     console.log(`Hello ${name}, You are ${age}, You are a ${gender}`);
 }
 
-sayHi(name, age, gender);
+sayHi(name, age);
 
 export {};
